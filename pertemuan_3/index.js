519d688363ed3a130c5f509fb49e24d38c6151cc
erlinafelis <erlinafelis@gmail.com>
2020-08-29 20:54:44 +0800

    first commit

---
+++ pertemuan_3/index.js
@@ -0,0 +1,28 @@
+/*
+ 1. variabel dan tipe data
+ */
+
+ const firstName = 'jhon';
+ let age = 20;
+ age =21;
+ const isMarried = true;
+
+ console.log(typeof firstName);
+ console.log(typeof age);
+ console.log(typeof isMarried);
+ console.log(age + " " + isMarried);
+
+ const info=
+    'my name is' + firstName +", i'm "+age+"years old" + isMarried;
+
+console.log(typeof info);
+
+
+/*
+        2.operator
+*/
+
+let bilangan = 10;
+
+console.log(bilangan == "10");  //loose equality
+console.log(bilangan === "10"); //strict equality
