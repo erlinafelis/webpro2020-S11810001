1e7266a3f3a2afe8cbd7a06c256da23c583c341f
erlinafelis <erlinafelis@gmail.com>
2020-09-03 18:59:45 +0800

    first commit

---
+++ pertemuan_4/index.js
@@ -0,0 +1,56 @@
+/*
+    1.function
+
+*/
+
+
+// let A=8;
+// let B=3;
+
+// let luasA= A*A;
+// let luasB=B*B;
+
+// let total= luasA+luasB;
+
+// console.log(total);
+
+
+// function penjumlahanDuaBujurSangkar (a,b){
+ 
+
+// let total= a*a+b*b;
+
+// return total;
+// }
+
+// console.log(penjumlahanDuaBujurSangkar(8,3));
+// console.log(penjumlahanDuaBujurSangkar(10,5));
+
+// function penjumlahanDuaBujurSangkar (a,b){
+ 
+//     return a*a+b*b;
+//     }
+    
+//     console.log(penjumlahanDuaBujurSangkar(8,3));
+//     console.log(penjumlahanDuaBujurSangkar(10,5));
+
+
+
+//     function tambah(a,b){
+//         return a+b;
+//     }
+//     function kali(a, b){
+//         return a*b;
+//     }
+
+//     console.log(tambah(10,5));
+//     console.log(tambah(kali(5,2),kali(10,2)));
+
+    const penjumlahanDuaBujurSangkar = function (a,b){
+ 
+        return a*a+b*b;
+        }
+        
+        console.log(penjumlahanDuaBujurSangkar(8,3));
+        console.log(penjumlahanDuaBujurSangkar(10,5));
+
