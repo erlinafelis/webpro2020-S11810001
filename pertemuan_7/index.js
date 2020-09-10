7d5ab0562a515cd513ce8ff1275257293006d83e
erlinafelis <erlinafelis@gmail.com>
2020-09-10 14:57:29 +0800

    third commit

---
+++ pertemuan_7/index.js
@@ -0,0 +1,139 @@
+/* if, elseif, else, // switch dan case */
+/* loop, in loop, off loop, while, do while*/
+
+// JavaScript conditional
+// menggunakan 2 cara if,else if, dan else
+//menggunakan switch dan case
+
+// cara if, else if, dan else
+
+let tanggal =22;
+
+if (tanggal > 20){
+    console.log("10 sampai 11 hari lagi akan mengakhiri akhir bulan");
+}else if(tanggal >=10 && tanggal <= 20){
+    console.log("pertengahan bulan");
+}else{
+    console.log
+        console.log("have a nice day");
+}
+
+//cth
+// let seragamSekolah;
+// let hari = "senin";
+
+// switch (hari) {
+//   case "senin":
+//     seragamSekolah = "kemeja putih bawahan merah";
+//     break;
+//   case "selasa":
+//     seragamSekolah = "kemeja hijau bawahan hitam";
+//     break;
+//   case "rabu":
+//     seragamSekolah = "kemeja putih bawahan putih";
+//     break;
+//   case "kamis":
+//     seragamSekolah = "kemeja batik merah bawahan hitam";
+//     break;
+//   case "jumat":
+//     seragamSekolah = "baju pramuka";
+//     break;
+//   default:
+//     seragamSekolah = "baju bebas";
+// }
+
+// console.log(seragamSekolah);
+//d
+// let seragamSekolah;
+// let hari = "jumat";
+
+// switch (hari) {
+//   case "senin":
+//   case "selasa":
+//   case "rabu":
+//   case "kamis":
+//     seragamSekolah = "kemeja putih bawahan merah";
+//     break;
+//   case "jumat":
+//     seragamSekolah = "baju pramuka";
+//     break;
+//   default:
+//     seragamSekolah = "baju bebas";
+// }
+
+// console.log(seragamSekolah);
+
+
+//LOOP
+ 
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+  
+
+
+// For/In Loop
+// const buku = {
+//     judul: "Harry Potter and The Philosopher's Stone",
+//     pengarang: "J. K. Rowling",
+//     tahun: 1997
+//  };
+//  for (x in buku) {
+//    console.log(x, ':', buku[x]);
+//  }
+ 
+
+ //For/Of Loop
+ //for (x of buku) menyatakan untuk setiap element x yang ada di variabel buku maka jalankan console.log(x);.
+
+
+//  const buku = ["Game of Thrones: A Song of Ice and Fire",  
+//    "Harry Potter and The Philosopher's Stone", 
+//    "Lord of The Rings: The Fellowship of The Ring"
+// ];
+
+// for (x of buku) {
+//   console.log(x);
+// }
+
+
+//While Loop
+//while berarti ketika kondisi yang ditentukan benar (true)
+//, maka jalankan semua kode yang ada di dalam {}.
+ 
+//cth
+let i = 5;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+//Do While Loop
+//cth
+let i = 7;
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 12);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
