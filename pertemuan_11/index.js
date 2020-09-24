6849e48f5e03ebbe468b9fc6eadc39baf4a477cc
erlinafelis <erlinafelis@gmail.com>
2020-09-24 14:16:50 +0800

    sixth commit

---
+++ pertemuan_11/index.js
@@ -0,0 +1,37 @@
+/* 
+        arrow function
+                            */
+
+
+// function ucapkanSalam() {
+// return "Selamat Pagi";
+// };
+
+// fungsi dengan parameter
+// const operasiPenjumlahan = (bilangan1, bilangan2) => {
+//     const hasil =  bilangan1 + bilangan2;
+//     return hasil;
+//   };
+  
+//   console.log(operasiPenjumlahan(12, 4)); 
+  
+  // fungsi yang tidak memiliki parameter
+//   const namaJenisBuah = () => {
+//     const buah = ["apel", "anggur", "jeruk",'melon'];
+//     return buah[Math.floor(Math.random()*(buah.length))];
+//   }
+  
+//   console.log(namaJenisBuah()); 
+  
+//IMPLICIT RETURN VALUE
+
+function bunga(nama) {
+   return `Hay ${nama}`;
+}
+ 
+console.log(bunga('mawar')); 
+
+ //cara singkat
+//  const bunga = (nama) => `Hay ${nama}`;
+
+console.log(bunga('melati'));
