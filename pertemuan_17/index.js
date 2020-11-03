c2ceec5d0ea6171deb7cda42c9144c756c70a51f
erlinafelis <erlinafelis@gmail.com>
2020-11-03 14:19:59 +0800

    10 commit

---
+++ pertemuan_17/index.js
@@ -0,0 +1,27 @@
+// const second = () => {
+//     const num = 10;
+//     setTimeout (() => {
+//         console.log('Async 1');
+//         console.log(num);
+//         setTimeout(()=> {
+//             console.log('Async 2');
+//             setTimeout(()=> {
+//                 console.log('Async 3');
+//             }, 2000)
+//         }, 2000)
+//     }, 2000, num)
+// }
+
+// const first = () => {
+//     console.log('First');
+//     second();
+//     console.log('The End');
+// }
+
+// first ();
+
+const second = () =>{
+    setTimeout (() => {
+        console.log('second async')
+    },4000)
+}
