2b773dd76b7fae06c3b92b17dd476c9aaa919b56
erlinafelis <erlinafelis@gmail.com>
2020-11-12 20:15:19 +0800

    Tugas

---
+++ pertemuan_20/src/Utils/person.js
@@ -0,0 +1,20 @@
+export const person = [
+    {
+        name: "camila ",
+        institution: "Unklab",
+        address: "manado",
+        phone: "088xxxxxxxxx"
+    },
+    {
+        name: "dylan ",
+        institution: "Unklab",
+        address: "Jakarta",
+        phone: "084xxxxxxxxx"
+    },
+    {
+        name: "indira",
+        institution: "Unklab",
+        address: "Tomohon",
+        phone: "087xxxxxxxxx"
+    },
+    ];
